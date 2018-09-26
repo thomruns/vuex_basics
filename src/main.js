e86851fca80d2d11448183f7b8a89a807d066c1b
Thom David <thomdavid74@gmail.com>
2018-09-26 11:28:38 -0700

    register store in root Vue instance

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { store } from './store/store'
+
+
 new Vue({
   el: '#app',
+  store, //ES6, same as store: store
   render: h => h(App)
 })
